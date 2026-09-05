2d99eae0fa1d881904bf92590b72d44e9db928dc
ben33880 <benj.moreau33@gmail.com>
2026-09-06 00:35:50 +0200

    Fix: Zoneless + provideHttpClient + polyfills cleanup for Angular 22

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes } from './app.routes';
@@ -6,7 +6,7 @@ import { apiPrefixInterceptor } from './core/interceptors/api-prefix.interceptor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([apiPrefixInterceptor]))
   ]
