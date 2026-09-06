fbffbbe0784d2e5607274bf0c62bfc8d0bda5a98
ben33880 <benj.moreau33@gmail.com>
2026-09-06 08:45:03 +0200

    Remove console.log from file-config service

--- frontend/src/app/services/file-config.service.ts
+++ frontend/src/app/services/file-config.service.ts
@@ -28,6 +28,8 @@ const DEFAULT_CONFIG: AppConfig = {
   }
 };
 
+const STORAGE_KEY = 'flowio_mqtt_config';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -49,10 +51,8 @@ export class FileConfigService {
       if (response.ok) {
         const loaded = await response.json();
         this.configSignal.set({ ...DEFAULT_CONFIG, ...loaded });
-        console.log('[Config] Loaded from /config.json', this.configSignal());
       }
-    } catch (e) {
-      console.warn('[Config] Failed to load /config.json, using defaults', e);
+    } catch {
       this.configSignal.set(DEFAULT_CONFIG);
     }
   }
@@ -64,12 +64,7 @@ export class FileConfigService {
       device: { ...current.device, ...newConfig.device }
     };
     
-    // In Docker setup, this would write to the mounted config.json
-    // For now, we update the signal and log the change
     this.configSignal.set(updated);
-    console.log('[Config] Updated config (would write to mounted volume):', updated);
-    
-    // Trigger a reload to get the updated file
     await this.loadConfig();
   }
 
